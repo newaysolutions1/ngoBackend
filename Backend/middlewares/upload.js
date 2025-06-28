8a5abdae5a927e2398f35425c6cee8c20856c6c7
newaysolutions1 <neway.solutions1@gmail.com>
2025-06-28 23:30:11 +0530

    changes

--- Backend/middlewares/upload.js
+++ Backend/middlewares/upload.js
@@ -3,29 +3,20 @@ const path = require('path');
 
 const uploadPath = path.join(__dirname, '../upload/students');
 
-
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
-    cb(null, `${Date.now()}-${file.originalname}`);
+    cb(null, Date.now() + '-' + file.originalname);
   },
 });
 
 const fileFilter = (req, file, cb) => {
-  const allowedTypes = [
-    'application/pdf',
-    'image/jpeg',
-    'image/jpg',
-    'image/png',
-    'video/mp4',
-  ];
-
-  if (allowedTypes.includes(file.mimetype)) {
+  if (file.mimetype === 'application/pdf' || 'application/jpeg' || 'application/jpg' || 'application/mp4') {
     cb(null, true);
   } else {
-    cb(new Error('Only PDF, image (jpeg/png), or mp4 files are allowed'), false);
+    cb(new Error('Only PDF files are allowed'), false);
   }
 };
 
